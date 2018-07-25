4117a2d46c3791f4bf2f8e5a2ac74178fef698d6
lorrainejy <lorrainejy@163.com>
2018-07-25 18:43:59 +0800

    7.25

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,20 +30,46 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%" alt="">
     </div>
-    <div class="detail" v-show="detailShow">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
+    <transition name="fade">
+      <div class="detail" v-show="detailShow" transition='fade'>
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size='48' :score='seller.score'></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item,index) in seller.supports" :key='item.index'>
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="hideDetail()">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
+
   </div>
 </template>  
 
 <script>
+import star from '../star/star'
 export default {
   // 父传子
   props: {
@@ -59,10 +85,16 @@ export default {
   methods: {
     showDetail () {
       this.detailShow = true
+    },
+    hideDetail () {
+      this.detailShow = false
     }
   },
   created () {
     this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+  },
+  components: {
+    star
   }
 }
 </script>
@@ -226,10 +258,17 @@ export default {
     z-index 100
     width 100%
     height 100%
-    overflow auto
+    overflow auto  
+    opacity 1
     background-color rgba(7,17,27,0.8)
+    &.fade-enter-active,&.fade-leave-active{
+      transition all 0.5s
+    }
+    &.fade-enter,&.fade-leave-active{
+      opacity 0
+      background-color rgba(7,17,27,0)
+    }
     .detail-wrapper{
-
       min-height 100%
       width 100%
       .detail-main{
@@ -241,6 +280,77 @@ export default {
           font-size 16px
           font-weight 700
         }
+        .star-wrapper{
+          margin-top 18px
+          padding 2px 0
+          text-align center
+        }
+        .title{
+          width 80%
+          margin 28px auto 24px auto 
+          display flex
+          .line{
+            flex 1
+            position relative
+            top -6px
+            border-bottom 1px solid rgba(255,255,255,0.2) 
+          }
+          .text{
+            padding 0 12px
+            font-weight 700
+            font-size 14px
+          }
+        }
+        .supports{
+          width 80%
+          margin 0 auto 
+          .support-item{
+            padding 0 12px
+            margin-bottom 12px
+            font-size 0
+            &:last-child{
+              margin-bottom 0
+            }
+            .icon{
+              display inline-block
+              width 16px
+              height 16px
+              vertical-align top
+              margin-right 6px
+              background-repeat no-repeat
+              background-size 16px 16px
+              &.decrease {
+                bg-image('decrease_2');
+              }
+              &.discount {
+                bg-image('discount_2');
+              }
+              &.guarantee {
+                bg-image('guarantee_2');
+              }
+              &.invoice {
+                bg-image('invoice_2');
+              }
+              &.special {
+                bg-image('special_2');
+              }
+            }
+            .text{
+              font-size 12px
+              line-height 16px
+            }
+          
+          }
+        }
+        .bulletin{
+          width 80%
+          margin 0 auto
+          .content{
+            padding: 0 12px
+            line-height: 24px
+            font-size: 12px
+          }
+        }
       }
     }
     .detail-close{
@@ -250,6 +360,7 @@ export default {
       margin:-64px auto 0 auto 
       clear both
       font-size 32px
+
     }
   }
 }
--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -1,25 +1,45 @@
 <template>
   <div class="star" :class="starType">
-    <span v-for="(itemClass,index) in itemsClasses" :key="index" :class="itemClass" class="star-item"></span>
+    <span v-for="(itemClass,index) in itemClasses" :key="index" :class="itemClass" class="star-item"></span>
   
   </div>
 </template>
 <script>
-  export default{
-    props:{
-      size:{
-        type:Number
-      },
-      score:{
-        type:Number
-      }
+const LENGTH = 5
+const CLS_ON = 'on'
+const CLS_HALF = 'half'
+const CLS_OFF = 'off'
+export default{
+  props: {
+    size: {
+      type: Number
     },
-    computed:{
-      starType(){
-        return 'star-'+this.size;
+    score: {
+      type: Number
+    }
+  },
+  computed: {
+    starType () {
+      return 'star-' + this.size
+    },
+    itemClasses () {
+      let result = []
+      let score = Math.floor(this.score * 2) / 2
+      let hasDecimal = score % 1 !== 0
+      let integer = Math.floor(score)
+      for (let index = 0; index < integer; index++) {
+        result.push(CLS_ON)  
       }
+      if (hasDecimal) {
+        result.push(CLS_HALF)  
+      }
+      while (result.length < LENGTH) {
+        result.push(CLS_OFF) 
+      }
+      return result
     }
   }
+}
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 @import '../../common/stylus/mixin';
